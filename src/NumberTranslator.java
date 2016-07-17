310bfbd7126f6044cc80e0b3f8c37eead65d80b0
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-07-17 13:24:47 -0400

    fixes

--- src/NumberTranslator.java
+++ src/NumberTranslator.java
@@ -1,102 +1,143 @@
-/**
- * 99 bottles of beer
- */
-public class NumberTranslator {
-    public static String toWord(int number) {
 
-        int tens = number / 10;
-        int ones = number % 10;
-        String tensAsText;
-        String onesAsText;
+public class NumberTranslator {
+
+    public static void rhyme(int number) {
+
         String word = "bottles";
 
         while (number > 0) {
-
-
-            if (number < 1) {
-                return "zero";
-            }
             if (number == 1) {
                 word = "bottle";
             }
-            System.out.print(number + " " + word + " of beer on the wall");
-            System.out.println(", " + number + " " + word + " of beer.");
+            String numberInWords = numberToWords(number);
+            System.out.print(Character.toUpperCase(numberInWords.charAt(0))
+                    + numberInWords.substring(1)
+                    + " " + word + " of beer on the wall");
+            System.out.println(", " + numberInWords + " " + word + " of beer.");
             System.out.print("Take one down and ");
             System.out.print("pass it around, ");
             number = number - 1;
 
+            numberInWords = numberToWords(number);
             if (number > 0) {
-                System.out.println(number + " " + word + " of beer on the wall.");
+                System.out.println(numberInWords + " " + word + " of beer on the wall.");
                 System.out.println(" ");
             } else {
                 System.out.println("no more bottles of beer on the wall.");
             }
         }
 
-            switch (tens) {
-                case 2:
-                    tensAsText = "twenty ";
+    }
+
+    public static String numberToWords(int number) {
+        int tens = number / 10;
+        int ones = number % 10;
+        String tensAsText;
+        String onesAsText;
+
+        if (number >= 11 && number <= 19) {
+            String t;
+            switch (number) {
+                case 11:
+                    t = "eleven";
                     break;
-                case 3:
-                    tensAsText = "thirty ";
+                case 12:
+                    t = "twelve";
                     break;
-                case 4:
-                    tensAsText = "forty ";
+                case 13:
+                    t = "thirteen";
                     break;
-                case 5:
-                    tensAsText = "fifty ";
+                case 14:
+                    t = "fourteen";
                     break;
-                case 6:
-                    tensAsText = "sixty ";
+                case 15:
+                    t = "fifteen";
                     break;
-                case 7:
-                    tensAsText = "seventy ";
+                case 16:
+                    t = "sixteen";
                     break;
-                case 8:
-                    tensAsText = "eighty ";
+                case 17:
+                    t = "seventeen";
                     break;
-                case 9:
-                    tensAsText = "ninety ";
+                case 18:
+                    t = "eighteen";
+                    break;
+                case 19:
+                    t = "nineteen";
                     break;
                 default:
-                    tensAsText = "";
-                    break;
+                    t = "";
             }
 
-            switch (ones) {
-                case 1:
-                    onesAsText = "one ";
-                    break;
-                case 2:
-                    onesAsText = "two ";
-                    break;
-                case 3:
-                    onesAsText = "three ";
-                    break;
-                case 4:
-                    onesAsText = "four ";
-                    break;
-                case 5:
-                    onesAsText = "five ";
-                    break;
-                case 6:
-                    onesAsText = "six ";
-                    break;
-                case 7:
-                    onesAsText = "seven ";
-                    break;
-                case 8:
-                    onesAsText = "eight ";
-                    break;
-                case 9:
-                    onesAsText = "nine ";
-                    break;
-                default:
-                    onesAsText = "";
-                    break;
-            }
-            return tensAsText + "" + onesAsText;
+            return t;
+        }
 
+        switch (tens) {
+            case 1:
+                tensAsText = "ten ";
+                break;
+            case 2:
+                tensAsText = "twenty ";
+                break;
+            case 3:
+                tensAsText = "thirty ";
+                break;
+            case 4:
+                tensAsText = "forty ";
+                break;
+            case 5:
+                tensAsText = "fifty ";
+                break;
+            case 6:
+                tensAsText = "sixty ";
+                break;
+            case 7:
+                tensAsText = "seventy ";
+                break;
+            case 8:
+                tensAsText = "eighty ";
+                break;
+            case 9:
+                tensAsText = "ninety ";
+                break;
+            default:
+                tensAsText = "";
+                break;
+        }
+
+        switch (ones) {
+            case 1:
+                onesAsText = "one ";
+                break;
+            case 2:
+                onesAsText = "two ";
+                break;
+            case 3:
+                onesAsText = "three ";
+                break;
+            case 4:
+                onesAsText = "four ";
+                break;
+            case 5:
+                onesAsText = "five ";
+                break;
+            case 6:
+                onesAsText = "six ";
+                break;
+            case 7:
+                onesAsText = "seven ";
+                break;
+            case 8:
+                onesAsText = "eight ";
+                break;
+            case 9:
+                onesAsText = "nine ";
+                break;
+            default:
+                onesAsText = "";
+                break;
+        }
+        return tensAsText + "" + onesAsText;
     }
 
 }
